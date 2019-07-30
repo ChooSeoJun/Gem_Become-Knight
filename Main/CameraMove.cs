32181b16c20e7ab898034a75c2b2933f86474bc7
OhChanHee <och5454@naver.com>
2019-07-30 23:07:58 +0900

    타이틀과 메인 캐릭터 이동 구현
    타이틀과 메인 캐릭터 이동 구현

---
+++ Main/CameraMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject Character;
+    Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Character.transform.position;
+        transform.Translate(0, 0, -10);
+        
+        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (camera.orthographicSize < 20)
+            {
+                camera.orthographicSize += 1;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (camera.orthographicSize > 5)
+            {
+                camera.orthographicSize -= 1;
+            }
+        }
+    }
+}
---
+++ Main/CharacterAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAction : MonoBehaviour
+{
+    public float Move_Speed = 10f;
+
+    void Start()
+    {
+            
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        Skill();
+        Attack();
+    }
+
+    void Skill()
+    {
+          
+    }
+
+    void Attack()
+    {
+        
+    }
+
+    void Move()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal") * Move_Speed * Time.deltaTime;
+        float yMove = Input.GetAxisRaw("Vertical") * Move_Speed * Time.deltaTime;
+        transform.position+=new Vector3(xMove,yMove);
+
+        Vector2 mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+
+        transform.up = mpos;
+    }
+}
