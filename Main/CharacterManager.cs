90f07f715a09d28efa552e27281968ce634676b6
OhChanHee <och5454@naver.com>
2019-07-31 01:18:08 +0900

    캐릭터 매니저 추가
    캐릭터매니저 추가

---
+++ Main/CharacterManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : ScriptableObject
+{
+    public static CharacterManager instance;
+
+    public int Ch_Hp = 3;
+    public int Ch_Power = 0;
+    public float Ch_Speed = 5f;
+
+
+    public static CharacterManager Get_instance()
+    {
+        if (instance == null)
+        {
+            instance = CreateInstance<CharacterManager>();
+        }
+
+        return instance;
+    }
+}
