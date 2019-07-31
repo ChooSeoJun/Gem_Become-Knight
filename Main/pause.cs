b34136e2d1b8098c974bc47f60fa65e7ea42d263
HyeonFe <45704002+HyeonFe@users.noreply.github.com>
2019-08-01 02:58:51 +0900

    퍼즈

---
+++ Main/pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+	public GameObject Pause;
+	bool isP = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+		Pause.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (!isP)
+			{
+				Time.timeScale = 0;
+				Pause.SetActive(true);
+				isP = true;
+			}
+			else
+			{
+				Time.timeScale = 1;
+				Pause.SetActive(false);
+				isP = false;
+			}
+		}
+    }
+}
