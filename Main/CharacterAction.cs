799250005558a5c537a83213278aa08ed751fc42
OhChanHee <och5454@naver.com>
2019-07-31 16:13:23 +0900

    바꼈음
    바꼇다고

--- Main/CharacterAction.cs
+++ Main/CharacterAction.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class CharacterAction : MonoBehaviour
 {
+    public GameObject Hand;
+    public GameObject Sword;
+    
+    private bool isAttacking = false;
 
     void Start()
     {
             
     }
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         Move();
         Skill();
@@ -24,7 +28,40 @@ public class CharacterAction : MonoBehaviour
 
     void Attack()
     {
-        
+        /*
+        Quaternion quaternion = new Quaternion();
+
+
+        if (Input.GetMouseButton(0))
+        {
+            Sword.SetActive(true);
+            Hand.transform.rotation = quaternion;
+          //  Sword.SetActive(false);
+        }
+        */
+
+        if (Input.GetMouseButton(0) && !isAttacking)
+        {
+            isAttacking = true;
+        }
+
+        if (isAttacking)
+        {
+
+            //Hand.transform.Rotate(new Vector3(0, 0, -15));
+            Hand.transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation, 20f);
+
+            Debug.Log(transform.rotation.eulerAngles.z);
+           if (Hand.transform.rotation.eulerAngles.z > transform.rotation.eulerAngles.z+90)
+            {
+                Debug.Log("ddd");
+                Hand.transform.rotation = Quaternion.Euler(0, 0, 90);
+                //Hand.transform.Rotate(0, 0, 90);
+                isAttacking = false;
+            }
+        }
+
+         //   Hand.transform.rotation
     }
 
     public void Move()
@@ -32,8 +69,9 @@ public class CharacterAction : MonoBehaviour
         float xMove = Input.GetAxisRaw("Horizontal") *CharacterManager.Get_instance().Ch_Speed * Time.deltaTime;
         float yMove = Input.GetAxisRaw("Vertical") * CharacterManager.Get_instance().Ch_Speed * Time.deltaTime;
         transform.position+=new Vector3(xMove,yMove);
-
-        Vector2 mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+        Hand.transform.position = transform.position;
+        
+       Vector2 mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
 
         transform.up = mpos;
 
