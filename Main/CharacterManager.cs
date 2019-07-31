11d7318ae907f72adaa1c08d5d9647ed8da6870f
OhChanHee <och5454@naver.com>
2019-07-31 23:02:18 +0900

    캐릭터매니저 변수추가

--- Main/CharacterManager.cs
+++ Main/CharacterManager.cs
@@ -7,9 +7,11 @@ public class CharacterManager : ScriptableObject
     public static CharacterManager instance;
 
     public int Ch_Hp = 3;
-    public int Ch_Power = 0;
+    public int Ch_Power = 2;
     public float Ch_Speed = 5f;
+    public int wave = 1;
     public int money = 0;
+    public int liveMon = 20;
 
     public static CharacterManager Get_instance()
     {
